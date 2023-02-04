82972c7b61818ae2063f30926f1d1f1c3602363d
Zintis May <zintismay@gmail.com>
2023-02-04 11:09:56 -0500

    small updates

--- README.md
+++ README.md
@@ -8,3 +8,6 @@
 // node ./server.js
 // go to (use) https://localhost:3333/50-50
 // use that endpoint as an inconsistent actor, so that you can work through promises, and error handling
+
+// If you access this page from the same server, you avoid CORS errors.
+// Highly recommend that you serve the working HTML pages as express routes
--- server.js
+++ server.js
@@ -1,9 +1,78 @@
 const express = require("express");
 const server = express();
+const PORT = 3333;
+
+let list = ["apples"];
+
+let counter = 0;
+
+server.use(express.json());
 
 server.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
+
+// server.get("/dirname", function (req, res) {
+//   res.send(__dirname);
+// });
+
+server.get("/req", function (req, res) {
+  res.json(Object.keys(req));
+});
+
+server.get("/res", function (req, res) {
+  res.json(Object.keys(res));
+});
+
+server.get("/list", function (req, res) {
+  res.json(list);
+});
+
+server.get("/addToList/:item", function (req, res) {
+  const item = req.params.item;
+  console.log(item);
+  list.push(item);
+  res.json(list);
+});
+
+server.get("/removeFromList/:itemToRemove", function (req, res) {
+  const itemToRemove = req.params.itemToRemove;
+  list = list.filter(function (item) {
+    return item !== itemToRemove;
+  });
+  res.json(list);
+});
+
+server.get("/query", function (req, res) {
+  const query = req.query;
+  res.json(query);
+});
+
+server.get("/params/:a/:b/:c", function (req, res) {
+  const params = req.params;
+  console.log(params);
+  res.json(params);
+});
+
+server.post("/post", function (req, res) {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+// server.get("/counter", function (req, res) {
+//   let value = String(counter);
+//   res.send(value);
+// });
+
+// server.get("/increment", function (req, res) {
+//   let value = String(++counter);
+//   res.send(value);
+// });
+
+// server.get("/decrement", function (req, res) {
+//   let value = String(--counter);
+//   res.send(value);
+// });
 
 server.get("/50-50", function (req, res) {
   const flip = rand(0, 2);
@@ -17,14 +86,19 @@ server.get("/50-50", function (req, res) {
     });
   }
 });
+
 server.get("/100", function (req, res) {
   res.status(200).send({
     message: "Success!",
   });
 });
 
-server.listen("3333", function (err) {
-  console.log(err);
+server.get("*", function (req, res) {
+  res.sendFile(__dirname + "/404.html");
+});
+
+server.listen(PORT, function () {
+  console.log(`Now listening on Port: ${PORT}`);
 });
 
 function rand(lo, hi) {
